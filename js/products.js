638dd1c72a86d0bf5e65123c2c1d0af5a73da307
Nicole07v <nicolefirulo02@gmail.com>
2024-09-10 18:19:50 -0300

    filters_js

--- js/products.js
+++ js/products.js
@@ -1,42 +1,123 @@
-document.addEventListener("DOMContentLoaded", (e) => {
-  showSpinner(); 
-  hideSpinner(); 
+const ORDER_ASC_BY_PRICE = "MINMAX";
+const ORDER_DESC_BY_PRICE = "MAXMIN";
+const ORDER_BY_PROD_SOLDCOUNT = "VENDIDOS";
+let currentProductsArray = [];
+let currentSortCriteria = undefined;
+let minPrice = undefined;
+let maxPrice = undefined;
+let catName = "";
 
-  const obtenerProducts = (products, catName) => {
-      const productsContainer = document.getElementById("products-container");
-      productsContainer.innerHTML = ''; 
-     
-      products.forEach(product => {
-          const productCard = document.createElement("div");
-          productCard.className = "pb-5 container product-item";
-          productCard.dataset.productId = product.id;
+function sortProducts(criteria, array) {
+    let result = [];
+    if (criteria === ORDER_ASC_BY_PRICE) 
+        {
+        result = array.sort((a, b) => (a.cost) - (b.cost));
+    } else if (criteria === ORDER_DESC_BY_PRICE) 
+        {
+        result = array.sort((a, b) => (b.cost) - (a.cost));
+    } else if (criteria === ORDER_BY_PROD_SOLDCOUNT) 
+        {
+        result = array.sort((a, b) => b.soldCount - a.soldCount);
+    }
+    return result;
+}
 
-          productCard.innerHTML = `
-          <div class="card mb-4 shadow-sm custom-card cursor-active">
-          <div class="card-body d-flex align-items-start">
-          <div class="image-container">
-             <img class="product-image" src="${product.image}" alt="${product.name}">
-          </div>
-          <div class="text-container ml-3">
-            <h3 class="m-3">${product.name}</h3>
-            <p class="card-text">${product.description}</p>
-            <p class="card-text precio"><strong>Costo:</strong> ${product.cost} ${product.currency}</p>
-            <div class="vendidos-container">
-            <p class="card-text"><strong>Vendidos:</strong> ${product.soldCount}</p>
-          </div>
-          `;
-          productsContainer.appendChild(productCard);
-      });
-      document.querySelector('h2.display-7').textContent = catName;
-      selectedProduct();
-  };
 
-  getJSONData(PRODUCTS_URL +localStorage.getItem("catID")+ EXT_TYPE)
-  .then(result => {
-      if (result.status === 'ok') {
-          obtenerProducts(result.data.products, result.data.catName); 
-      }
-  });
+
+function showProductsList(){
+    let htmlContentToAppend = "";
+    for(let i = 0; i < currentProductsArray.length; i++){
+        let product = currentProductsArray[i];
+
+        
+        if (((minPrice == undefined) || (minPrice != undefined && parseFloat(product.cost) >= minPrice)) &&
+            ((maxPrice == undefined) || (maxPrice != undefined && parseFloat(product.cost) <= maxPrice))) {
+
+            htmlContentToAppend += `
+            <div class="pb-5 container">
+                <div class="card mb-4 shadow-sm custom-card cursor-active">
+                    <div class="card-body d-flex align-items-start">
+                        <div class="image-container">
+                            <img class="product-image" src="${product.image}" alt="${product.name}">
+                        </div>
+                        <div class="text-container ml-3">
+                            <h3 class="m-3">${product.name}</h3>
+                            <p class="card-text">${product.description}</p>
+                            <p class="card-text precio"><strong>Costo:</strong> ${product.cost} ${product.currency}</p>
+                            <div class="vendidos-container">
+                                <p class="card-text"><strong>Vendidos:</strong> ${product.soldCount}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `;
+        }
+    }
+    document.getElementById("products-container").innerHTML = htmlContentToAppend;
+    document.querySelector('h2.display-7').textContent = catName; 
+}
+
+function sortAndShowProducts(sortCriteria, productsArray) {
+    currentSortCriteria = sortCriteria;
+    currentProductsArray = productsArray || currentProductsArray;
+    currentProductsArray = sortProducts(currentSortCriteria, currentProductsArray);
+    showProductsList();
+}
+
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL + localStorage.getItem("catID") + EXT_TYPE).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            currentProductsArray = resultObj.data.products;
+            catName = resultObj.data.catName; 
+            showProductsList();
+        }
+    });
+
+    document.getElementById("sortAscPrice").addEventListener("click", function() {
+        sortAndShowProducts(ORDER_ASC_BY_PRICE);
+    });
+
+    document.getElementById("sortDescPrice").addEventListener("click", function() {
+        sortAndShowProducts(ORDER_DESC_BY_PRICE);
+    });
+
+    document.getElementById("sortBySoldCount").addEventListener("click", function() {
+        sortAndShowProducts(ORDER_BY_PROD_SOLDCOUNT);
+    });
+
+    document.getElementById("btnClearFilter").addEventListener("click", function() {
+        document.getElementById("minPrice").value = "";
+        document.getElementById("maxPrice").value = "";
+
+        minPrice = undefined;
+        maxPrice = undefined;
+
+        showProductsList();
+    });
+
+    document.getElementById("btnPriceFilter").addEventListener("click", function(){
+        minPrice = document.getElementById("minPrice").value;
+        maxPrice = document.getElementById("maxPrice").value;
+
+        if ((minPrice != undefined) && (minPrice != "") && (parseFloat(minPrice)) >= 0){
+            minPrice = parseFloat(minPrice);
+        }
+        else{
+            minPrice = undefined;
+        }
+
+        if ((maxPrice != undefined) && (maxPrice != "") && (parseFloat(maxPrice)) >= 0){
+            maxPrice = parseFloat(maxPrice);
+        }
+        else{
+            maxPrice = undefined;
+        }
+
+        showProductsList();
+    });
+
 
   //Guarda en localStorage el ID del producto seleccionado y redirige a product-info
   function selectedProduct() {
