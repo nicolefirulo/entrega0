d5c09181393d1e20187cada8805bdc82f19f5a3b
Agustina <agustinaosanocorbo@gmail.com>
2024-10-14 17:25:34 -0300

    desafiate entrega 5

---
+++ js/my-profile.js
@@ -0,0 +1,41 @@
+function saveImg() {
+    const fileInput = document.getElementById('imageInput'); //Obtiene el elemento de entrada del archivo
+    const file = fileInput.files[0];
+    document.getElementById('preview')
+
+    if (file) {
+        const reader = new FileReader(); //Crea un objeto FileReader(), el cual se encarga de leer el archivo
+
+        reader.onloadend = () => {
+            let base64String = reader.result; //El contenido del archivo se convierte en una cadena de texto en formato Base64
+            console.log(base64String);
+            localStorage.setItem('imageBase64', base64String); //Guarda en localStorage la cadena Base64
+            showImg(); //Muestra la imagen
+        };
+
+        reader.onerror = (error) => {
+            console.error('Error de FileReader:', error);
+        };
+
+        reader.readAsDataURL(file);
+    } else {
+        console.error('No se ha seleccionado ningún archivo');
+    }
+}
+
+function showImg() {
+    let base64String = localStorage.getItem("imageBase64");
+    if (base64String) {
+        document.getElementById('preview').setAttribute("src", base64String);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    showImg();
+
+    const btnSave = document.getElementById("saveChanges");
+    btnSave.addEventListener("click", () => {
+        saveImg();
+    });
+    
+});
