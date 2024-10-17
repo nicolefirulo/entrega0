7120af46fb4f40856a0c32c1d926a836fae50267
josefinacabrera1 <josefinacabreragomez@gmail.com>
2024-10-17 18:33:04 -0300

    change_Dark-Mode

--- js/init.js
+++ js/init.js
@@ -58,3 +58,24 @@ function login_check() {
 }
 
 login_check();
+
+ // Lógica de modo oscuro
+ const btnSwitch = document.querySelector('#switch');
+
+ // Cargar preferencia de modo oscuro desde localStorage
+ if (localStorage.getItem('dark-mode') === 'true') {
+     document.body.classList.add('dark');
+     btnSwitch.classList.add('active');
+ }
+
+ btnSwitch.addEventListener('click', () => {
+     document.body.classList.toggle('dark');
+     btnSwitch.classList.toggle('active');
+
+     // Guardar el estado del modo oscuro en localStorage
+     if (document.body.classList.contains('dark')) {
+         localStorage.setItem('dark-mode', 'true');
+     } else {
+         localStorage.setItem('dark-mode', 'false');
+     }
+ });
--- js/products.js
+++ js/products.js
@@ -150,24 +150,3 @@ document.getElementById("searchBar").addEventListener("input", function() {
     filterProducts(searchTerm);  
 
 });
-
- // Lógica de modo oscuro
- const btnSwitch = document.querySelector('#switch');
-
- // Cargar preferencia de modo oscuro desde localStorage
- if (localStorage.getItem('dark-mode') === 'true') {
-     document.body.classList.add('dark');
-     btnSwitch.classList.add('active');
- }
-
- btnSwitch.addEventListener('click', () => {
-     document.body.classList.toggle('dark');
-     btnSwitch.classList.toggle('active');
-
-     // Guardar el estado del modo oscuro en localStorage
-     if (document.body.classList.contains('dark')) {
-         localStorage.setItem('dark-mode', 'true');
-     } else {
-         localStorage.setItem('dark-mode', 'false');
-     }
- });
