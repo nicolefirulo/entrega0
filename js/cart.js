a68e87ccccbc34f76f2181c91b6416fbb3dbe46a
josefinacabrera1 <josefinacabreragomez@gmail.com>
2024-11-15 17:18:03 -0300

    desafiante_entrega7

--- js/cart.js
+++ js/cart.js
@@ -9,6 +9,15 @@ function convertToUSD(amount, currency) {
   return amount * exchangeRates[currency];
 }
 
+// Función para eliminar un producto
+function eliminarProducto(index) {
+  cart.splice(index, 1);
+  localStorage.setItem("carrito", JSON.stringify(cart)); 
+  cartProducts();
+  actualizarBadgeCarrito();
+  updateCosts();
+}
+
 function cartProducts() {
   const buyingForm = document.getElementById("buyingForm");
   const productCard = document.getElementById("cartProducts");
@@ -41,7 +50,7 @@ function cartProducts() {
                 <button class="btn btn-warning decrease-btn" data-index="${i}">-</button>
                 <input type="number" name="cantidad" class="form-control quantity-input mx-1 text-center" data-index="${i}" value="${product.cantidad}" min="1">
                 <button class="btn btn-warning increase-btn" data-index="${i}">+</button>
-                <button class="btn btn-dark"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
+                <button class="btn btn-dark" onclick="eliminarProducto(${i})"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
             </div>
               <hr class="col-12 d-block d-sm-none"><p class="d-block d-sm-none d-md-none d-lg-none col-4 sub-text">Subtotal</p>
               <p class="col-4 col-sm-3 col-md-3 col-lg-2 subtotal">${subtotal} ${product.currency}</p>  
